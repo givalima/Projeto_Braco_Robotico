865135a3a3a1e2d1c778b7721f95c38931b1f605
Givanildo Lima <glnjunior@ic.ufal.br>
2018-09-20 17:39:35 -0300

    Versão para montar a base de imagens

--- SkeletonBasics.h
+++ SkeletonBasics.h
@@ -3,7 +3,7 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
-
+ 
 #pragma once
 
 #include "resource.h"
